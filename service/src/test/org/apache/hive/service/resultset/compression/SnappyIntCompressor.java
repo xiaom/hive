9bc07aca8618160e2aba37616823a66945d24905
Xiaojian Wang <glancky@gmail.com>
2015-06-24 16:38:12 -0700

    Format code

--- service/src/test/org/apache/hive/service/resultset/compression/SnappyIntCompressor.java
+++ service/src/test/org/apache/hive/service/resultset/compression/SnappyIntCompressor.java
@@ -8,56 +8,55 @@ import org.xerial.snappy.Snappy;
 
 public class SnappyIntCompressor implements ColumnCompressor {
 
-	/**
-	   * Before calling the compress() method below, should check if the column is compressable by any
-	   * given plugin.
-	   * 
-	   * @Param col Column to be checked
-	   * @return boolean which says yes/no
-	   */
-	  public boolean isCompressible(Column col) {
-		System.out.println("Yo! I can compress integers!!");
-	    switch (col.getType()) {
-	      case INT_TYPE:
-	        return true;
-	      default:
-	        return false;
-	    }
-	  }
+  /**
+   * Before calling the compress() method below, should check if the column is compressable by any
+   * given plugin.
+   * 
+   * @Param col Column to be checked
+   * @return boolean which says yes/no
+   */
+  public boolean isCompressible(Column col) {
+    System.out.println("Yo! I can compress integers!!");
+    switch (col.getType()) {
+      case INT_TYPE:
+        return true;
+      default:
+        return false;
+    }
+  }
 
-	  /*
-	   * Method provided for testing purposes. Decompresses a compressed byte array. should be compressed using 
-	   * the compress() function below which uses the snappy JAR files. If Snappy JAR file isn't used, this method 
-	   * will return the wrong result. 
-	   */
-	  public static int[] decompress(byte[] inp) throws IOException {
-		  return Snappy.uncompressIntArray(inp);
-	  }
-	  
-	  /**
-	   * Compresses a given column.
-	   * 
-	   * @Param col A column to be compressed
-	   * @return byte array with compressed data
-	   */
-	  public byte[] compress(Column col) {
-		System.out.println("ECBS called me! I am the compress method!!");
-	    switch (col.getType()) {
-	      case INT_TYPE:
-	        return Snappy.compress(col.getInts());
-	      default:
-	        return new byte[0];
-	    }
-	  }
+  /*
+   * Method provided for testing purposes. Decompresses a compressed byte array. should be
+   * compressed using the compress() function below which uses the snappy JAR files. If Snappy JAR
+   * file isn't used, this method will return the wrong result.
+   */
+  public static int[] decompress(byte[] inp) throws IOException {
+    return Snappy.uncompressIntArray(inp);
+  }
 
-	  @Override
-	  public String getCompressorSet() {
-	    return "pluginsnappy";
-	  }
+  /**
+   * Compresses a given column.
+   * 
+   * @Param col A column to be compressed
+   * @return byte array with compressed data
+   */
+  public byte[] compress(Column col) {
+    System.out.println("ECBS called me! I am the compress method!!");
+    switch (col.getType()) {
+      case INT_TYPE:
+        return Snappy.compress(col.getInts());
+      default:
+        return new byte[0];
+    }
+  }
 
-	  @Override
-	  public String getVendor() {
-	    return "org.apache.hive.service.cli";
-	  }
+  @Override
+  public String getCompressorSet() {
+    return "pluginsnappy";
+  }
 
+  @Override
+  public String getVendor() {
+    return "org.apache.hive.service.cli";
+  }
 }
--- service/src/test/org/apache/hive/service/resultset/compression/TestEncodedColumnBasedSet.java
+++ service/src/test/org/apache/hive/service/resultset/compression/TestEncodedColumnBasedSet.java
@@ -1,85 +1,74 @@
 package org.apache.hive.service.resultset.compression;
 
 import static org.junit.Assert.*;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hive.service.cli.Column;
 import org.apache.hive.service.cli.EncodedColumnBasedSet;
 import org.apache.hive.service.cli.Type;
-import org.apache.hive.service.cli.thrift.TEnColumn;
 import org.apache.hive.service.cli.thrift.TRowSet;
-import org.apache.hive.service.cli.thrift.TTypeId;
-import org.apache.thrift.protocol.TType;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
-
 /*
  * To test functionality of EncodedColumnBasedSets. 
  */
 public class TestEncodedColumnBasedSet {
-	
-	
-	private static TRowSet testTRowSet;
-	private static Column testColumn;
-	private static EncodedColumnBasedSet ecbs;
-	private static HiveConf hiveConf = new HiveConf();
-	private static JSONObject info;
-	private static JSONObject stringJSON;
-	
-	
-	@Before
-	public void setUp() throws JSONException {
-		//create a TRowSet with one Column, which is of type INT. and has values {0, 1, 2}. and 
-		testTRowSet = new TRowSet();
-		testTRowSet.setStartRowOffset((long)0);
-		testColumn = new Column(Type.INT_TYPE);
-		info = new JSONObject();
-		stringJSON = new JSONObject();
-		testColumn.addValue(Type.INT_TYPE,0);
-		testColumn.addValue(Type.INT_TYPE,1);
-		testColumn.addValue(Type.INT_TYPE,2);
-		testTRowSet.addToColumns(testColumn.toTColumn());
-	}
-	
 
-	@Test
-	public void positive() throws IOException, JSONException {
-		//send the right information as part of compressorInfo. EncodedColumnBasedSet should compress the data.
-		info.put("vendor", "org.apache.hive.service.cli");
-		info.put("compressorSet", "pluginsnappy");
-		stringJSON.put("INT_TYPE", info);
-		hiveConf.set("CompressorInfo", stringJSON.toString());
-		ecbs = new EncodedColumnBasedSet(testTRowSet);
-		ecbs.setConf(hiveConf);
-		TRowSet compressed = ecbs.toTRowSet();
-		assertEquals(ecbs.getColumns().size(), 1);
-		byte[] compressedData = compressed.getEnColumns().get(0).getEnData();
-		assertArrayEquals(SnappyIntCompressor.decompress(compressedData), new int[]{0, 1, 2});	
-	}
-	
-	@Test
-	public void negative() throws IOException, JSONException {
-		//put the wrong information in compressorInfo. EncodedColumnBasedSet should send back the column uncompressed. 
-		info.put("vendor", "org.apache.hive.service.cli");
-		info.put("compressorSet", "thisiswrong");
-		stringJSON.put("INT_TYPE", info);
-		hiveConf.set("CompressorInfo", stringJSON.toString());
-		ecbs = new EncodedColumnBasedSet(testTRowSet);
-		ecbs.setConf(hiveConf);
-		TRowSet compressed = ecbs.toTRowSet();
-		assertEquals(ecbs.getColumns().size(), 1);
-		assertEquals(compressed.getEnColumnsSize(), 0);
-		Integer[] l = new Integer[]{0, 1, 2};
-		assertArrayEquals(compressed.getColumns().get(0).getI32Val().getValues().toArray(), l);
-	}
-	
+  private static TRowSet testTRowSet;
+  private static Column testColumn;
+  private static EncodedColumnBasedSet ecbs;
+  private static HiveConf hiveConf = new HiveConf();
+  private static JSONObject info;
+  private static JSONObject stringJSON;
+
+  @Before
+  public void setUp() throws JSONException {
+    // create a TRowSet with one Column, which is of type INT. and has
+    // values {0, 1, 2}. and
+    testTRowSet = new TRowSet();
+    testTRowSet.setStartRowOffset((long) 0);
+    testColumn = new Column(Type.INT_TYPE);
+    info = new JSONObject();
+    stringJSON = new JSONObject();
+    testColumn.addValue(Type.INT_TYPE, 0);
+    testColumn.addValue(Type.INT_TYPE, 1);
+    testColumn.addValue(Type.INT_TYPE, 2);
+    testTRowSet.addToColumns(testColumn.toTColumn());
+  }
+
+  @Test
+  public void positive() throws IOException, JSONException {
+    // send the right information as part of compressorInfo.
+    // EncodedColumnBasedSet should compress the data.
+    info.put("vendor", "org.apache.hive.service.cli");
+    info.put("compressorSet", "pluginsnappy");
+    stringJSON.put("INT_TYPE", info);
+    hiveConf.set("CompressorInfo", stringJSON.toString());
+    ecbs = new EncodedColumnBasedSet(testTRowSet);
+    ecbs.setConf(hiveConf);
+    TRowSet compressed = ecbs.toTRowSet();
+    assertEquals(ecbs.getColumns().size(), 1);
+    byte[] compressedData = compressed.getEnColumns().get(0).getEnData();
+    assertArrayEquals(SnappyIntCompressor.decompress(compressedData), new int[] { 0, 1, 2 });
+  }
+
+  @Test
+  public void negative() throws IOException, JSONException {
+    // put the wrong information in compressorInfo. EncodedColumnBasedSet
+    // should send back the column uncompressed.
+    info.put("vendor", "org.apache.hive.service.cli");
+    info.put("compressorSet", "thisiswrong");
+    stringJSON.put("INT_TYPE", info);
+    hiveConf.set("CompressorInfo", stringJSON.toString());
+    ecbs = new EncodedColumnBasedSet(testTRowSet);
+    ecbs.setConf(hiveConf);
+    TRowSet compressed = ecbs.toTRowSet();
+    assertEquals(ecbs.getColumns().size(), 1);
+    assertEquals(compressed.getEnColumnsSize(), 0);
+    Integer[] l = new Integer[] { 0, 1, 2 };
+    assertArrayEquals(compressed.getColumns().get(0).getI32Val().getValues().toArray(), l);
+  }
 }
